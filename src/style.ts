08c3278b93d073e96e8d2f734c2be098def2dc02
Cesar Peres <cperes84@gmail.com>
2024-07-02 19:43:02 -0300

    style: adicionando tipagem no container

--- src/style.ts
+++ src/style.ts
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.div<{ type: string }>`
   color: #fff;
   display: flex;
   justify-content: center;
